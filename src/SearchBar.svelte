7f878a17bb49340b0441565a5efa7a7465f40717
Allison Wagner <allisonjw01@gmail.com>
2020-02-29 15:17:14 -0700

    Adds basic styling to search bar and button

--- src/SearchBar.svelte
+++ src/SearchBar.svelte
@@ -11,14 +11,30 @@
     border-radius: 4px;
     border: 1px solid #ccc;
     padding: 10px 20px;
-    font-size: 20px;
+    font-size: 1.7em;
     margin-bottom: 50px;
   }
+
+  .search_btn {
+    background: grey;
+    color: #333;
+    font-size: 1.7em;
+    border: none;
+    cursor: pointer;
+    outline: none;
+  }
+
+  .search_btn:hover {
+    opacity: 0.8;
+    transition: opacity 0.2s ease-in;
+  }
+
 </style>
 
 <div class="search">
-  <form class="search-form" on:submit|preventDefault={handleSubmit}>
+  <form class="search-form">
     <input bind:value={search} class="search-input" type="search"
     placeholder="Search Artwork by Name" />
+    <button class="search_btn" on:submit|preventDefault={handleSubmit}>Search</button>
   </form>
 </div>
