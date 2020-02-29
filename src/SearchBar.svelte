dd9bc40c74ff2cc413eb8d072bf63484ba541c37
Allison Wagner <allisonjw01@gmail.com>
2020-02-29 15:07:30 -0700

    Adds basic form and search bar artwork and will pass props from app for handleSubmit and search

--- src/SearchBar.svelte
+++ src/SearchBar.svelte
@@ -1,1 +1,24 @@
-<div>search component</div>
+<script>
+  export let search;
+  export let handleSubmit;
+  // export this functions from App
+</script>
+
+<style>
+  .search-input {
+    width: 100%;
+    max-width: 800px;
+    border-radius: 4px;
+    border: 1px solid #ccc;
+    padding: 10px 20px;
+    font-size: 20px;
+    margin-bottom: 50px;
+  }
+</style>
+
+<div class="search">
+  <form class="search-form" on:submit|preventDefault={handleSubmit}>
+    <input bind:value={search} class="search-input" type="search"
+    placeholder="Search Artwork by Name" />
+  </form>
+</div>
