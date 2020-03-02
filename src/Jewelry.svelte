22fbc14ad874b0120c5fbbf02d7e92efbf449768
Allison Wagner <allisonjw01@gmail.com>
2020-03-01 18:00:44 -0700

    Passes prop and displays jewelry

--- src/Jewelry.svelte
+++ src/Jewelry.svelte
@@ -1,9 +1,11 @@
 <script>
-
+export let jewelry;
 </script>
 
-<style>
-
-</style>
-
-<h2>Jewelry</h2>
+<article class="art_piece">
+  <h3>Title: {jewelry.title}</h3>
+  <h3>Dated: {jewelry.dated}</h3>
+  <h3>Division: {jewelry.division}</h3>
+  <h3 class="url_link">{jewelry.url}</h3>
+  <img class="art" src={jewelry.primaryimageurl || "https://homestaymatch.com/images/no-image-available.png"} alt={jewelry.commentary || ""}>
+</article>
