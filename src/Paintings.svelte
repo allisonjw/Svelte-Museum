960516c50abf0a9f4bf2e77e1318774383ed75a4
Allison Wagner <allisonjw01@gmail.com>
2020-03-01 17:59:52 -0700

    Passes prop and displays paintings

--- src/Paintings.svelte
+++ src/Paintings.svelte
@@ -1,9 +1,11 @@
 <script>
-
+export let painting;
 </script>
 
-<style>
-
-</style>
-
-<h2>Paintings</h2>
+<article class="art_piece">
+  <h3>Title: {painting.title}</h3>
+  <h3>Dated: {painting.dated}</h3>
+  <h3>Division: {painting.division}</h3>
+  <h3 class="url_link">{painting.url}</h3>
+  <img class="art" src={painting.primaryimageurl || "https://homestaymatch.com/images/no-image-available.png"} alt={painting.commentary || ""}>
+</article>
