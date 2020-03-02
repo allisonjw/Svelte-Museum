c18415c106692c234bdffeb0be4715290c81dec9
Allison Wagner <allisonjw01@gmail.com>
2020-03-01 18:19:41 -0700

    Passes prop and displays sculptures

--- src/Sculptures.svelte
+++ src/Sculptures.svelte
@@ -2,8 +2,10 @@
 export let sculpture;
 </script>
 
-<style>
-
-</style>
-
-<h2>Sculptures</h2>
+<article class="art_piece">
+  <h3>Title: {sculpture.title}</h3>
+  <h3>Dated: {sculpture.dated}</h3>
+  <h3>Division: {sculpture.division}</h3>
+  <h3 class="url_link">{sculpture.url}</h3>
+  <img class="art" src={sculpture.primaryimageurl || "https://homestaymatch.com/images/no-image-available.png"} alt={sculpture.commentary || ""}>
+</article>
