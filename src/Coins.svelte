81f448f269f00c6ba8e173cece92723d70c5d5fb
Allison Wagner <allisonjw01@gmail.com>
2020-03-03 16:36:52 -0700

    Adding pop up modal to coins and fixing some styling

--- src/Coins.svelte
+++ src/Coins.svelte
@@ -1,13 +1,40 @@
 <script>
-export let coin;
+  export let coin;
+  import Modal from './Modal.svelte';
+
+  let showModal = false;
 </script>
 <style>
     div {
         margin: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: normal;
+    }
+    h4 {
+        margin: 2px;
+    }
+    .modal_img {
+        height: 100%;
+        width: 100%;
+    }
+    button {
+        background-color: grey;
+        color: #333;
+        width: 50%;
+        border: 1px solid gray;
+        /* margin: auto; */
+    }
+    button:hover {
+        opacity: 0.8;
+        transform: scale(1.1);
+        transition: all .1s ease-in-out;
     }
 </style>
-
 <article class="art_piece">
+<button on:click="{() => showModal = true}">
+	See Larger Image
+  </button>
   <div>
     <h3>Title: {coin.title}</h3>
     <h3>Dated: {coin.dated}</h3>
@@ -16,3 +43,8 @@ export let coin;
   </div>
   <img class="art" src={coin.primaryimageurl || "https://homestaymatch.com/images/no-image-available.png"} alt={coin.commentary || ""}>
 </article>
+{#if showModal}
+	<Modal on:close="{() => showModal = false}">
+		<img class="modal_img" src={coin.primaryimageurl || "https://homestaymatch.com/images/no-image-available.png"} alt={coin.commentary || ""}>
+	</Modal>
+{/if}
--- src/Photographs.svelte
+++ src/Photographs.svelte
@@ -8,27 +8,44 @@
 <style>
     div {
         margin: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: normal;
+    }
+    h4 {
+        margin: 2px;
     }
     .modal_img {
         height: 100%;
         width: 100%;
     }
+    button {
+        background-color: grey;
+        color: #333;
+        width: 50%;
+        border: 1px solid gray;
+        /* margin: auto; */
+    }
+    button:hover {
+        opacity: 0.8;
+        transform: scale(1.1);
+        transition: all .1s ease-in-out;
+    }
 </style>
 
 <article class="art_piece">
-  <div>
-    <h3>Title: {photograph.title}</h3>
-    <h3>Artist/Name: {photograph.people[0].displayname}</h3>
-    <h3>Dated: {photograph.dated}</h3>
-    <h3>Division: {photograph.division}</h3>
-    <h3 class="url_link">{photograph.url}</h3>
-  </div>
   <button on:click="{() => showModal = true}">
 	See Larger Image
   </button>
+  <div>
+    <h4>Title: {photograph.title}</h4>
+    <h4>Artist/Name: {photograph.people[0].displayname}</h4>
+    <h4>Dated: {photograph.dated}</h4>
+    <h4>Division: {photograph.division}</h4>
+    <h4 class="url_link">{photograph.url}</h4>
+  </div>
   <img class="art" src={photograph.primaryimageurl || "https://homestaymatch.com/images/no-image-available.png"} alt={photograph.commentary || ""}>
 </article>
-
 {#if showModal}
 	<Modal on:close="{() => showModal = false}">
 		<img class="modal_img" src={photograph.primaryimageurl || "https://homestaymatch.com/images/no-image-available.png"} alt={photograph.commentary || ""}>
