dd5108e41ed8beaf39b8925170532e48c39d0730
Allison Wagner <allisonjw01@gmail.com>
2020-03-03 17:08:39 -0700

    Adds the pop up modal and removes extra div

--- src/Paintings.svelte
+++ src/Paintings.svelte
@@ -1,14 +1,43 @@
 <script>
-export let painting;
+  export let painting;
+  import Modal from './Modal.svelte';
+
+  let showModal = false;
 </script>
 
-<style>
-    div {
+<style>    div {
         margin: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: normal;
     }
+    h3 {
+        margin: 2px;
+    }
+    .modal_img {
+        height: 100%;
+        width: 100%;
+    }
+    button {
+        background-color: #109e6c;
+        color: #333;
+        width: 50%;
+        border: 1px solid gray;
+        border-radius: 3px;
+        font-size: 1.2em;
+    }
+    button:hover {
+        opacity: 0.8;
+        transform: scale(1.1);
+        transition: all .1s ease-in-out;
+    }
+
 </style>
 
 <article class="art_piece">
+  <button on:click="{() => showModal = true}">
+	See Larger Image
+  </button>
   <div>
     <h3>Title: {painting.title}</h3>
     <!-- <h3>Artist/Name: {painting.people[0].displayname}</h3> -->
@@ -18,3 +47,8 @@ export let painting;
   </div>
   <img class="art" src={painting.primaryimageurl || "https://homestaymatch.com/images/no-image-available.png"} alt={painting.commentary || ""}>
 </article>
+{#if showModal}
+	<Modal on:close="{() => showModal = false}">
+		<img class="modal_img" src={painting.primaryimageurl || "https://homestaymatch.com/images/no-image-available.png"} alt={painting.commentary || ""}>
+	</Modal>
+{/if}
